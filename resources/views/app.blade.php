4c64aab3c2ec497be35b99de11160b8aaca2cc56
Jake Vivian <jacob@sadsoft.com>
2022-07-13 20:49:50 -0700

    Starting to look more like pingcrm.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
         <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
         <!-- Scripts -->
         @routes
