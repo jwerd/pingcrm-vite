ab09a7b62d6176c29d20ededc7c41478ae26907a
Jake Vivian <jacob@sadsoft.com>
2022-07-15 20:00:06 -0700

    Ziggy MainMenu

--- resources/js/Components/MainMenu.vue
+++ resources/js/Components/MainMenu.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
         <div class="mb-4">
-            <Link class="group flex items-center py-3" href="/">
+            <Link class="group flex items-center py-3" :href="route('dashboard')">
                 <icon name="dashboard" class="mr-2 w-4 h-4" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
                 <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
             </Link>
         </div>
         <div class="mb-4">
-            <Link class="group flex items-center py-3" href="/organizations">
+            <Link class="group flex items-center py-3" :href="route('organizations')">
                 <icon name="office" class="mr-2 w-4 h-4" :class="isUrl('organizations') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
                 <div :class="isUrl('organizations') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Organizations</div>
             </Link>
         </div>
         <div class="mb-4">
-            <Link class="group flex items-center py-3" href="/contacts">
+            <Link class="group flex items-center py-3" :href="route('contacts')">
                 <icon name="users" class="mr-2 w-4 h-4" :class="isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
                 <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Contacts</div>
             </Link>
         </div>
         <div class="mb-4">
-            <Link class="group flex items-center py-3" href="/reports">
+            <Link class="group flex items-center py-3" :href="route('reports')">
                 <icon name="printer" class="mr-2 w-4 h-4" :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
                 <div :class="isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Reports</div>
             </Link>
